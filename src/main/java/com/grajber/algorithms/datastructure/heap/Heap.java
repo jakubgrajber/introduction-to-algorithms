314a963704dd32d47574a9606d39417dd359195a
Jakub Grajber <grajber.jakub@gmail.com>
2023-04-13 06:48:14 +0200

    Max-Heapify procedure

---
+++ src/main/java/com/grajber/algorithms/datastructure/heap/Heap.java
@@ -0,0 +1,58 @@
+package com.grajber.algorithms.datastructure.heap;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Heap<T extends Comparable<T>> {
+
+    private final T[] heap;
+    private final int capacity;
+    private int size;
+
+    public Heap(int capacity) {
+        this.size = 0;
+        this.capacity = capacity;
+        this.heap = (T[]) new Comparable[capacity+1];
+    }
+
+    public boolean insert(T element) {
+        if (size >= capacity)
+            return false;
+        heap[size+1] = element;
+        size++;
+        return true;
+    }
+
+    public void maxHeapify(int root) {
+        int left = left(root);
+        int right = right(root);
+        int largest;
+        if (left <= size && heap[left].compareTo(heap[root]) > 0)
+            largest = left;
+        else
+            largest = root;
+        if (right <= size && heap[right].compareTo(heap[root]) > 0)
+            largest = right;
+        if (largest != root) {
+            var temp = heap[largest];
+            heap[largest] = heap[root];
+            heap[root] = temp;
+            maxHeapify(largest);
+        }
+    }
+
+    public void print() {
+        System.out.println(Arrays.toString(heap));
+    }
+    public int parent(int position) {
+        return position >> 1;
+    }
+
+    public int left(int position) {
+        return position << 1;
+    }
+
+    public int right(int position) {
+        return (position << 1) + 1;
+    }
+}
